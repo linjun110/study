b9b9f1455a1a8e03c542a5dce544e979e723828c
linjun110 <linjun110@gmail.com>
2016-04-03 00:59:17 +0800

    add mvn

---
+++ maven/java/javaViaMvn/src/main/java/com/linjun/javaViaMvn/App.java
@@ -0,0 +1,13 @@
+package com.linjun.javaViaMvn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
