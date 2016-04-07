dce865bb665928a5ce2ab09392984ff4d05bdafd
linjun <linjun@amazon.com>
2016-04-07 17:35:57 +0800

    rename some files

--- Java/springMvcWeb/src/main/java/com/linjun/java/springMvcWeb/controller/MyController.java
+++ Java/springMvcWeb/src/main/java/com/linjun/java/springMvcWeb/controller/MyController.java
@@ -1,4 +1,4 @@
-package com.linjun.java.springMvcWeb;
+package com.linjun.java.springMvcWeb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
