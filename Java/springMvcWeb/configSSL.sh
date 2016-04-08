45c486e097e93000b8787353047c1928ea8f7f54
linjun <linjun@amazon.com>
2016-04-08 13:52:27 +0800

    config tomcat to SSL

--- Java/springMvcWeb/configSSL.sh
+++ Java/springMvcWeb/configSSL.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 keytool -genkey -alias tomcat -keyalg RSA
 # .keystore file is store in the dir where the command is executed.
+
+# ref: http://tomcat.apache.org/tomcat-8.0-doc/ssl-howto.html
+# beware: the password should be the same during creating keystore
+
+<Connector port="8081" protocol="org.apache.coyote.http11.Http11NioProtocol"
+maxThreads="200"
+scheme="https" secure="true" SSLEnabled="true"
+keystoreFile="${user.home}/.keystore" keystorePass="linjunAmazon"
+clientAuth="false" sslProtocol="TLS"
+connectionTimeout="20000"
+redirectPort="8443" />
