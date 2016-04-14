2738be91d2167f2aae6b20a738aa48b7b1ced6a4
linjun <linjun@amazon.com>
2016-04-14 21:12:07 +0800

    add animation

--- Java/springMvcWeb/src/main/webapp/js/auth.js
+++ Java/springMvcWeb/src/main/webapp/js/auth.js
@@ -1,10 +1,35 @@
 function logout() {
     document.getElementById("logoutForm").submit();
 }
+function animateGo() {
+    $("#guitailang").attr("src", "images/guitailang.gif");
+    $("#guitailang").animate({
+        marginRight:"800px"
+    }, {
+        duration: 6000,
+        complete: function(){
+            setTimeout(animateBack, 1000);
+        }
+    });
+}
+
+function animateBack() {
+    $("#guitailang").attr("src", "images/haomaru.gif");
+    $("#guitailang").animate({
+        marginRight:"0px"
+    }, {
+        duration: 6000,
+        complete: function(){
+            setTimeout(animateGo, 1000);
+        }
+    });
+}
 
 $(document).ready(function(){
     $("#register").click(function(){
         window.location.href = "register"
     });
     $("#logout").click(logout);
+
+    animateGo();
 });
